01f9e739a7b4c959ec16df5e80bbe375c7f265cf
Saleh Yaish <salehyaish99@gmail.com>
2026-06-09 07:09:18 -0700

    .

--- drone_gas_core/launch/full_system.launch.py
+++ drone_gas_core/launch/full_system.launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, SetParameter, TimerAction
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, TimerAction
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from ament_index_python.packages import get_package_share_directory
 import os
 
